de76f1cfadb6980518b483fab240498095650994
Amy Lin <alin@192-168-1-107.tpgi.com.au>
2016-07-02 16:52:31 +1000

    Initial Commit

---
+++ iPray/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPray
+//
+//  Created by Amy Lin on 2/07/2016.
+//  Copyright © 2016 Amy Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
